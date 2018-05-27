068b91204dda626849411ea0c2b0f60c69b60010
wakkymen <komor1313@gmail.com>
2018-05-28 01:19:01 +0200

    romved mocks from App.js, improved perfomrance a bit and added spinner animation when fetching

--- src/jsx-json/jsxToJson.js
+++ src/jsx-json/jsxToJson.js
@@ -1,5 +1,4 @@
 import React from "react";
-import shortid from "shortid";
 
 export function jsxToJson(object) {
   return JSON.stringify(object, (key, value) => {
@@ -30,18 +29,4 @@ export function processJson(object, components) {
   }
   const {children, ...props} = object.props;
   return typeof children === "string" ? React.createElement(object.type, props, children) : React.createElement(object.type, props, processJson(children, components));
-}
-
-export function jsonToJsx(jsonString, components) {
-  return JSON.parse(jsonString, (key, value) => {
-    if (key === "$$typeof" && value === "react" ){
-      return Symbol.for("react.element");
-    } else if (key === "type" && typeof value === "string" && value.substr(0, 8) === "function") {
-      return components[value.substr(8,1).toLocaleLowerCase()+value.substr(9, value.length-8)];
-    } else if (key === "key") {
-      return shortid.generate();
-    } else {
-      return value;
-    }
-  });
 }
