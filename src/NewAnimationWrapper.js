057f03591d2e9345565431a6cb50417c9e8034a5
wakkymen <komor1313@gmail.com>
2018-06-11 10:15:48 +0200

    readme changed + file delete

--- src/NewAnimationWrapper.js
+++
@@ -1,7 +0,0 @@
-import CardNew from "./CardNew";
-import withUnfoldAnimationInterface from "./components/animations/withUnfoldAnimationInterface";
-
-
-
-
-export default withUnfoldAnimationInterface(CardNew);
